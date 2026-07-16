21a8ed652e377da9c046ab64dc6e9b9f11ea01e0
AaronMcCar <aaronwmccarthy@gmail.com>
2026-07-16 01:02:29 -0400

    A4P1 Test 5

--- assignments/assignment-4/assignment-4-part-1/main.js
+++ assignments/assignment-4/assignment-4-part-1/main.js
@@ -5,8 +5,8 @@ const generateBtn = document.querySelector(".generate");
 const story = document.querySelector(".story");
 
 function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
-  return array[random];
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
 }
 
 // Raw text strings
@@ -16,13 +16,13 @@ const places = ["the soup kitchen", "Disneyland", "the White House"];
 const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
 
 function returnRandomStoryString() {
-  const randomCharacter = randomValueFromArray(characters);
-  const randomPlace = randomValueFromArray(places);
-  const randomEvent = randomValueFromArray(events);
+    const randomCharacter = randomValueFromArray(characters);
+    const randomPlace = randomValueFromArray(places);
+    const randomEvent = randomValueFromArray(events);
 
-  let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
+    let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
 
-  return storyText;
+    return storyText;
 }
 
 // Event listener and partial generate function definition
@@ -30,18 +30,20 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
-  if (customName.value !== "") {
-    const name = customName.value;
-    newStory = newStory.replace("Bob", name);
-  }
+    let newStory = returnRandomStoryString();
 
-  if (document.getElementById("uk").checked) {
-    const weight = `${Math.round(300/14)} stone`;
-    const temperature = `${Math.round((94-32)*(5/9))} Celsius`;
-    newStory = newStory.replace("300 pounds", weight);
-    newStory = newStory.replace("94 Fahrenheit", temperature);
-  }
+    if (customName.value !== "") {
+        const name = customName.value;
+        newStory = newStory.replace("Bob", name);
+    }
 
-  story.textContent = newStory;
-  story.style.visibility = "visible";
+    if (document.getElementById("uk").checked) {
+        const weight = `${Math.round(300/14)} stone`;
+        const temperature = `${Math.round((94-32)*(5/9))} Celsius`;
+        newStory = newStory.replace("300 pounds", weight);
+        newStory = newStory.replace("94 Fahrenheit", temperature);
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = "visible";
 }
