ce2be21b7435f771700cd7f601c581c5d1667f2b
AaronMcCar <aaronwmccarthy@gmail.com>
2026-07-16 02:00:32 -0400

    A4P1 Test 7
    Added markup to index.html for A4P1.

--- assignments/assignment-4/assignment-4-part-1/main.js
+++ assignments/assignment-4/assignment-4-part-1/main.js
@@ -1,3 +1,4 @@
+
 // Complete variable definitions and random functions
 
 const customName = document.getElementById("custom-name");
@@ -14,6 +15,8 @@ function randomValueFromArray(array) {
 const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const places = ["the soup kitchen", "Disneyland", "the White House"];
 const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
+
+// Partial return random string function
 
 function returnRandomStoryString() {
     const randomCharacter = randomValueFromArray(characters);
