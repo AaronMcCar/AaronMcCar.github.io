cc1da4c859eb8d14bb3ec8cfaab4da67ef33f628
AaronMcCar <aaronwmccarthy@gmail.com>
2026-07-28 09:01:37 -0400

    Update to A4P3

--- assignments/assignment-4/assignment-4-part-3/main.js
+++ assignments/assignment-4/assignment-4-part-3/main.js
@@ -1,6 +1,6 @@
-// setup canvasc
+// setup canvas
 
-onst canvas = document.querySelector("canvas");
+const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
